96f8564e58d6f9e58822f448d6a32eb5692993e3
shahdDaghash <78550854+shahdDaghash@users.noreply.github.com>
2021-11-22 15:28:57 +0300

    Create 958. Check Completeness of a Binary Tree.cpp

---
+++ 958. Check Completeness of a Binary Tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+public:
+    bool isCompleteTree(TreeNode* root) {
+        bool pre = true;
+        
+        queue <TreeNode*> q;
+        q.push(root);
+        
+        
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+            
+            if(node==NULL){
+                pre = false;
+            }
+            else{
+                if(pre==false){
+                    return false;
+                }
+                else{
+                    q.push(node->left);
+                    q.push(node->right);
+                }
+            }
+        }
+        return true;
+    }
+};
