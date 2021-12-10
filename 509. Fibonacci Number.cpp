ffbeb3c49536275d669f825d872071acd7ff1023
shahdDaghash <78550854+shahdDaghash@users.noreply.github.com>
2021-12-10 19:31:58 +0300

    Create 509. Fibonacci Number.cpp

---
+++ 509. Fibonacci Number.cpp
@@ -0,0 +1,17 @@
+//dp bottom up solution
+class Solution {
+public:
+    int fib(int n) {
+        if(n < 2){
+            return n;
+        }
+        
+        int a = 0, b = 1, c;
+        for(int i=2; i<=n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
